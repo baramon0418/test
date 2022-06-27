6510b828d32a15917e4b7aac7f88ffe27a88fd05
baramon0418 <108270144+baramon0418@users.noreply.github.com>
2022-06-27 22:34:08 +0900

    Add files via upload

---
+++ 제어문을 이용한 구구단 프로그래밍.m
@@ -0,0 +1,43 @@
+for (dan=1:9)
+%      for ele=1:9
+%     fprintf('%d*%d=%d\n',dan,ele,dan*ele);
+%      end
+  switch(dan)
+      case 1
+             for ele=1:9
+                fprintf('%d*%d=%d\n',dan,ele,dan*ele);
+             end
+      case 2
+             for ele=1:9
+                fprintf('%d*%d=%d\n',dan,ele,dan*ele);
+             end
+      case 3
+             for ele=1:9
+                fprintf('%d*%d=%d\n',dan,ele,dan*ele);
+             end
+      case 4
+             for ele=1:9
+                fprintf('%d*%d=%d\n',dan,ele,dan*ele);
+             end
+      case 5
+             for ele=1:9
+                fprintf('%d*%d=%d\n',dan,ele,dan*ele);
+             end
+      case 6
+             for ele=1:9
+                fprintf('%d*%d=%d\n',dan,ele,dan*ele);
+             end
+      case 7
+              for ele=1:9
+                fprintf('%d*%d=%d\n',dan,ele,dan*ele);
+              end
+      case 8
+              for ele=1:9
+                fprintf('%d*%d=%d\n',dan,ele,dan*ele);
+              end
+      case 9
+              for ele=1:9
+                fprintf('%d*%d=%d\n',dan,ele,dan*ele);
+              end
+  end
+end
